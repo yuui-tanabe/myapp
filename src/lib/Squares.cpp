0cc1d9660d410b16a655c96f3e1325f9f37f2ec1
Yuui Tanabe <tanabeyuui721@icloud.com>
2019-02-08 14:21:22 +0900

    Update Squares.cpp

--- src/lib/Squares.cpp
+++ src/lib/Squares.cpp
@@ -2,13 +2,14 @@
 #include "Squares.hpp"
 
 using namespace std;
-int s = (a+b+c+d)/2;
 namespace Quadruple {
 int Brahmagupta(int a,int b,int c,int d) {
+    int s = (a+b+c+d)/2;
     int square1 = sqrt((s-a)*(s-b)*(s-c)*(s-d));
     cout << square1 << endl;
     }
 int Bretschneider(int w,int x,int y,int z,int angle1,int angle2) {
+    int s = (w+x+y+z)/2;
     if ((angle1+angle2) == 180) {
         Brahmagupta(w,x,y,z);
     }

622a268eefedf161aa3117db0563e4c1922691d8
Yuui Tanabe <tanabeyuui721@icloud.com>
2019-02-08 14:37:03 +0900

    Update Squares.cpp

--- src/lib/Squares.cpp
+++ src/lib/Squares.cpp
@@ -7,7 +7,7 @@ int Brahmagupta(int a,int b,int c,int d) {
     int s = (a+b+c+d)/2;
     int square1 = sqrt((s-a)*(s-b)*(s-c)*(s-d));
     cout << square1 << endl;
-    return EXIT_SUCCESS
+    return EXIT_SUCCESS;
     }
 int Bretschneider(int w,int x,int y,int z,int angle1,int angle2) {
     int s = (w+x+y+z)/2;
@@ -17,6 +17,7 @@ int Bretschneider(int w,int x,int y,int z,int angle1,int angle2) {
         float radians =  (angle1+angle2)*(M_PI/180);
         int square2 = sqrt((s-w)*(s-x)*(s-y)*(s-z)-w*x*y*z*cos(radians));
         cout << square2 << endl;
+        return EXIT_SUCCESS;
         }
     }
 }
